c16da609b6e2e67b5db2b9f73364c9a223672f71
Anedvago <61481830+Anedvago@users.noreply.github.com>
2023-11-22 21:19:50 -0500

    creando navbar añadiendo el logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 export const routes: Routes = [
   {
-    path: 'contactenos',
-    loadChildren: () =>
-      import('./modules/contactenos/contactenos.routes').then((m) => m.routes),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./modules/inicio/inicio.routes').then((m) => m.routes),
+      },
+      {
+        path: 'contactenos',
+        loadChildren: () =>
+          import('./modules/contactenos/contactenos.routes').then(
+            (m) => m.routes
+          ),
+      },
+    ],
   },
 ];
